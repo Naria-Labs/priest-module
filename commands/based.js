5271b72940038b9db652341e2a838438ec9d7060
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-01 13:50:37 +0100

    added few files from develop pr-> master

---
+++ commands/based.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('basedlvl')
+		.setDescription('How based are you on the scale')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('Set a user that you want to see amazing based lvl')),
+
+	async execute(interaction) {
+		//red>>blue>>purple>>green
+		//base lvls
+		//maybe every 25 %?
+		const red = 0xff0000;
+		const blue = 0x0000ff;
+		const purple = 0x800080;
+		const green = 0x008000;
+		const userMentioned = interaction.options.getMember('user');
+		const userID = userMentioned.id;
+		const userString = parseInt(userID.toString().slice(-2));
+		const today = new Date().getDate();
+        
+		const ifBased = userString * today;
+		const basedLvl = parseInt(ifBased.toString().slice(-2));
+
+		let basedColor;
+		let text;
+
+		if (basedLvl >= 0 && basedLvl <= 25) {
+			basedColor = red;
+			text = 'You are not based at all. Try maybe in the next incarnation.';
+		} else if (basedLvl >= 26 && basedLvl <= 50) {
+			basedColor = blue;
+			text = 'You are based but not that much. Maybe try to appeal to the court of baseness.';
+		} else if (basedLvl >= 51 && basedLvl <= 75) {
+			basedColor = purple;
+			text = 'You are closer to basedness. Keep it up, you are so close.';
+		} else if (basedLvl >= 76 && basedLvl <= 100) {
+			basedColor = green;
+			text = 'Congratulations, you are so based that it\'s not possible to measure it.';
+		}
+
+		const basedEmbed = new EmbedBuilder()
+			.setColor(basedColor)
+			.setTitle('How based are you')
+			.setDescription(`User ${userMentioned}, how based is that person?`)
+			.addFields(
+				{ name: 'Basedness level', value: `${text}`, inline: true },
+			)
+			.setTimestamp();
+
+		await interaction.reply({ embeds: [basedEmbed] });
+	},
+};
--- module.js
+++ module.js
@@ -1,5 +1,15 @@
 module.exports = {
   commands: [
-    require('./utils/ping.js'),
+    require('./commands/embedInfo.js'),
+    require('./commands/gameTest.js'),
+    require('./commands/leaderboard.js'),
+    require('./commands/memeMessage.js'),
+    require('./commands/muteMessage.js'),
+    require('./commands/randomImageGenerator.js'),
+    require('./commands/textToMeme.js'),
+    require('./commands/privateList.js'),
+    require('./commands/help.js'),
+    require('./commands/based.js'),
+    require('./commands/4chanb.js'),
   ]
 }
