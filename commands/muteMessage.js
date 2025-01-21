463a3cbbb3c33b84f995c230ff3b12e3f9cbdda7
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-21 17:37:44 +0100

    test

--- commands/muteMessage.js
+++ commands/muteMessage.js
@@ -1,41 +1,41 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { hasGoodRole, goodRoles } = require('./discordCommands');
 
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('mute')
-		.setDescription('Server mute for set ammount of time')
+		.setDescription('Server mute for set amount of time')
 		.addUserOption(option =>
 			option.setName('user')
 				.setDescription('Find a user that you wanna mute')
 				.setRequired(true)
-				)
-        .addIntegerOption(option1 =>
-			option1.setName('time')
-                .setDescription('Time in minutes')
+		)
+		.addIntegerOption(option =>
+			option.setName('time')
+				.setDescription('Time in minutes')
 				.setRequired(true)
-				),
+		),
 
 	async execute(interaction) {
 		const userMentioned = interaction.options.getMember('user');
 		const time = interaction.options.getInteger('time');
 		const userID = interaction.member.id;
-		const unixTime = Math.floor((Date.now() / 1000) + `${time}` * 60);
 
 		if (!userMentioned.voice.channel) {
 			return interaction.reply({
-				context: `${userMentioned} is not in a voice channel`,
+				content: `${userMentioned} is not in a voice channel`,
 				ephemeral: true
 			});
-		} 
+		}
 
-		if (!hasGoodRole(userMentioned)) {
+		if (!hasGoodRole(interaction.member)) {
 			return interaction.reply({
 				content: `<@${userID}>, you can't server mute ${userMentioned} because you don't have a ${goodRoles.map(role => `<@&${role}>`).join(' or ')}`,
 				ephemeral: true,
 			});
 		}
+
+		const unixTime = Math.floor((Date.now() / 1000) + time * 60);
 
 		try {
 			await userMentioned.voice.setMute(true, 'You said some fucky wacky');
@@ -45,13 +45,15 @@ module.exports = {
 			});
 		} catch (error) {
 			console.error(error);
-			await interaction.reply('There was an error trying to server muted the user');
+			await interaction.reply('There was an error trying to server mute the user');
 		}
 
-		setTimeout(()=> {
-			userMentioned.voice.setMute(false, 'You will get away from saying it. For now');
-		},
-            `${time}` * 60000);
-
+		setTimeout(async () => {
+			try {
+				await userMentioned.voice.setMute(false, 'You will get away from saying it. For now');
+			} catch (error) {
+				console.error('Error unmuting the user:', error);
+			}
+		}, time * 60000);
 	},
 };
