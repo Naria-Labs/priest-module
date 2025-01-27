95d0e05c281b5880e4e169ef05a19061028ac429
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-27 19:46:59 +0100

    I give up

--- commands/sendMessage.js
+++ commands/sendMessage.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ComponentType } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const { hasGoodRole, goodRoles } = require('./discordCommands');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,6 +21,13 @@ module.exports = {
         const message = interaction.options.getString('message');
         const userThatSent = interaction.user;
 
+        if (!hasGoodRole(interaction.member)) {
+            return interaction.reply({
+                content: `<@${userID}>, you can't use this comannd because you don't have a ${goodRoles.map(role => `<@&${role}>`).join(' or ')}`,
+                ephemeral: true,
+            });
+        }
+
         const messageToUser = new EmbedBuilder()
             .setColor(0x003253)
             .setTitle(`You got a new mail from ${userThatSent.tag}`)
@@ -27,62 +36,13 @@ module.exports = {
             )
             .setTimestamp();
 
-        const buttons = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('reply')
-                    .setLabel('Reply')
-                    .setStyle('PRIMARY')
-            );
-
         try {
             const user = await interaction.client.users.fetch(userID);
-            const dmChannel = await user.createDM();
-            await dmChannel.send({ embeds: [messageToUser], components: [buttons] });
-
-            await interaction.reply({ content: 'Message sent successfully! Waiting for reply...', ephemeral: true });
-
-            const filter = i => i.customId === 'reply' && i.user.id === userID;
-            const collector = dmChannel.createMessageComponentCollector({ filter, componentType: ComponentType.Button, time: 60000 });
-
-            collector.on('collect', async i => {
-                if (i.customId === 'reply') {
-                    await i.deferReply({ ephemeral: true });
-                    await i.editReply({ content: 'Thinking...', ephemeral: true });
-
-                    const messageFilter = response => response.author.id === userID;
-                    const messageCollector = dmChannel.createMessageCollector({ filter: messageFilter, max: 1, time: 60000 });
-
-                    messageCollector.on('collect', async response => {
-                        console.log(`Collected message: ${response.content}`);
-                        const replyMessage = new EmbedBuilder()
-                            .setColor(0x003253)
-                            .setTitle(`You got a reply from ${response.author.tag}`)
-                            .addFields(
-                                { name: 'Reply', value: response.content, inline: true },
-                            )
-                            .setTimestamp();
-
-                        await userThatSent.send({ embeds: [replyMessage] });
-                        await response.reply({ content: `Your message is "${response.content}" and it has been sent!`, ephemeral: true });
-                    });
-
-                    messageCollector.on('end', collected => {
-                        if (collected.size === 0) {
-                            dmChannel.send('You did not reply in time.');
-                        }
-                    });
-                }
-            });
-
-            collector.on('end', collected => {
-                if (collected.size === 0) {
-                    dmChannel.send('You did not click the reply button in time.');
-                }
-            });
+            await user.send({ embeds: [messageToUser] });
+            await interaction.reply({ content: 'Message sent successfully!', ephemeral: true });
         } catch (error) {
             console.error('Error sending message:', error);
-            await interaction.reply({ content: `Failed to send the message. Error: ${error.message}`, ephemeral: true });
+            await interaction.reply({ content: 'Failed to send the message.', ephemeral: true });
         }
     },
 };
