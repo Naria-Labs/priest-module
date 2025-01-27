c45fdb8c9b593c2382444a7abd489b1945a32012
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-27 10:47:45 +0100

    message test

---
+++ commands/sendMessage.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('smessage')
+        .setDescription('Send a message to a user')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('Type a user you want to send a message')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('Type a message you want to send to the user')
+                .setRequired(true)),
+
+    async execute(interaction) {
+        const userMentioned = interaction.options.getUser('user');
+        const userID = userMentioned.id;
+        const message = interaction.options.getString('message');
+        const userThatSent = interaction.user;
+
+        const messageToUser = new EmbedBuilder()
+            .setColor('blue')
+            .setTitle(`You got a new mail from ${userThatSent.tag}`)
+            .addFields(
+                { name: 'Message', value: message, inline: true },
+            )
+            .setTimestamp();
+
+        try {
+            const user = await interaction.client.users.fetch(userID);
+            await user.send({ embeds: [messageToUser] });
+            await interaction.reply({ content: 'Message sent successfully!', ephemeral: true });
+        } catch (error) {
+            console.error('Error sending message:', error);
+            await interaction.reply({ content: 'Failed to send the message.', ephemeral: true });
+        }
+    },
+};
