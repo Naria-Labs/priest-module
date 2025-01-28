a8ce645592ede1bfd7e0c9ce78ff1fce1ec0db24
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-28 14:27:26 +0100

    fix ?

--- commands/debugCommand.js
+++ commands/debugCommand.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,7 +10,13 @@ module.exports = {
 		.setDescription('Debug command for last 2k chars'),
 
 	async execute(interaction) {
-		exec('cd naria-labs/rzulty-bot-priest && pm2 log develop-priest --lines 1000', (error, stdout, stderr) => {
+		const dirPath = path.join(__dirname, 'naria-labs/rzulty-bot-priest');
+
+		if (!fs.existsSync(dirPath)) {
+			return interaction.reply(`Error: Directory ${dirPath} does not exist.`);
+		}
+
+		exec(`cd ${dirPath} && pm2 log develop-priest --lines 1000`, (error, stdout, stderr) => {
 			if (error) {
 				console.error(`exec error: ${error}`);
 				return interaction.reply(`Error: ${error.message}`);
@@ -21,7 +29,7 @@ module.exports = {
 			const basedEmbed = new EmbedBuilder()
 				.setColor('#0099ff')
 				.setTitle('Log Output')
-				.setDescription(`\`\`\`${stdout.slice(-1984)}\`\`\``) //Slice to last 1984 characters
+				.setDescription(`\`\`\`${stdout.slice(-1984)}\`\`\``) // Slice to last 1984 characters
 				.setTimestamp();
 
 			interaction.reply({ embeds: [basedEmbed] });
