df9795fddc272e382a71902f30e36aa64c69c999
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-28 14:36:04 +0100

    aaa?

--- commands/debugCommand.js
+++ commands/debugCommand.js
@@ -8,14 +8,21 @@ module.exports = {
 		.setDescription('Debug command for last 2k chars'),
 
 	async execute(interaction) {
+		//Log the current working directory
+		const currentDir = process.cwd();
+		console.log(`Current working directory: ${currentDir}`);
+
+		//Reply with the current working directory for debugging
+		await interaction.reply(`Current working directory: ${currentDir}`);
+
 		exec('cd ../.. && cd naria-labs/rzulty-bot-priest && pm2 log develop-priest --lines 1000', (error, stdout, stderr) => {
 			if (error) {
 				console.error(`exec error: ${error}`);
-				return interaction.reply(`Error: ${error.message}`);
+				return interaction.followUp(`Error: ${error.message}`);
 			}
 			if (stderr) {
 				console.error(`stderr: ${stderr}`);
-				return interaction.reply(`Stderr: ${stderr}`);
+				return interaction.followUp(`Stderr: ${stderr}`);
 			}
 
 			const basedEmbed = new EmbedBuilder()
@@ -24,7 +31,7 @@ module.exports = {
 				.setDescription(`\`\`\`${stdout.slice(-1984)}\`\`\``) // Slice to last 1984 characters
 				.setTimestamp();
 
-			interaction.reply({ embeds: [basedEmbed] });
+			interaction.followUp({ embeds: [basedEmbed] });
 		});
 	},
 };
