6fc4a79b81fbbd2a47322ab44fd0395c1b0acb61
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-01-28 14:31:20 +0100

    aa?

--- commands/debugCommand.js
+++ commands/debugCommand.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,13 +8,7 @@ module.exports = {
 		.setDescription('Debug command for last 2k chars'),
 
 	async execute(interaction) {
-		const dirPath = '/home/debian/naria-labs/rzulty-bot-priest'; // Use the absolute path
-
-		if (!fs.existsSync(dirPath)) {
-			return interaction.reply(`Error: Directory ${dirPath} does not exist.`);
-		}
-
-		exec(`cd ${dirPath} && pm2 log develop-priest --lines 1000`, (error, stdout, stderr) => {
+		exec('cd ../.. && cd naria-labs/rzulty-bot-priest && pm2 log develop-priest --lines 1000', (error, stdout, stderr) => {
 			if (error) {
 				console.error(`exec error: ${error}`);
 				return interaction.reply(`Error: ${error.message}`);
