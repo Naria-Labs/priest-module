6274e3045568e38632208764277d472e160dd2c7
ThetaMTX <42809224+ThetaMTX@users.noreply.github.com>
2025-04-04 12:10:03 +0200

    maybe less is more

--- commands/sudoku.js
+++ commands/sudoku.js
@@ -19,6 +19,57 @@ function makeSubgroup(name) {
             );
 }
 
+function isValid(matrix, row, col, num) {
+    for (let x = 0; x < 9; x++) {
+        if (matrix[row][x] === num || matrix[x][col] === num) {
+            return false;
+        }
+    }
+
+    const startRow = row - row % 3;
+    const startCol = col - col % 3;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (matrix[i + startRow][j + startCol] === num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function fillSudoku(matrix) {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (matrix[row][col] === 0) {
+                for (let num = 1; num <= 9; num++) {
+                    if (isValid(matrix, row, col, num)) {
+                        matrix[row][col] = num;
+                        if (fillSudoku(matrix)) {
+                            return true;
+                        }
+                        matrix[row][col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function deleteNumbers(matrix, level) {
+    const numbersToDelete = level === 'easy' ? 22 : level === 'medium' ? 44 : level === 'hard' ? 55 : 66;
+    for (let i = 0; i < numbersToDelete; i++) {
+        const row = Math.floor(Math.random() * 9);
+        const col = Math.floor(Math.random() * 9);
+        matrix[row][col] = 0;
+    }
+    return matrix;
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sudoku')
@@ -34,45 +85,7 @@ module.exports = {
         const difficultyLevel = options.getSubcommand();
         const matrixSudoku = Array.from({ length: 9 }, () => Array(9).fill(0));
 
-        function shuffle(array) {
-            let i = array.length,
-                j = 0,
-                temp;
-
-            while (i--) {
-                j = Math.floor(Math.random() * (i + 1));
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-
-            return array;
-        }
-
-        const rowSudoku = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 9; j++) {
-                matrixSudoku[i][j] = rowSudoku[j];
-                for (let k = 0; k < j; k++) {
-                    if (matrixSudoku[i][j] === matrixSudoku[i][k]) {
-                        matrixSudoku[i][j] = rowSudoku[k];
-                    }
-                }
-            }
-            shuffle(rowSudoku);
-        }
-
-        function deleteNumbers(matrix, level) {
-            const numbersToDelete = level === 'easy' ? 40 : level === 'medium' ? 50 : level === 'hard' ? 60 : 70;
-            for (let i = 0; i < numbersToDelete; i++) {
-                const row = Math.floor(Math.random() * 9);
-                const col = Math.floor(Math.random() * 9);
-                matrix[row][col] = 0;
-            }
-            return matrix;
-        }
-
+        fillSudoku(matrixSudoku);
         const finalSudoku = deleteNumbers(matrixSudoku, difficultyGroup);
 
         await interaction.reply({ content: `You have selected ${difficulty[difficultyGroup]} with ${difficultyLevel} level`, ephemeral: true });
